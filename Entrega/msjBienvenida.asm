2b1706b5c0554726cb58768c01b7e10b2d564ff0
gaangaritav <gaangaritav@unal.edu.co>
2014-05-28 01:46:54 -0500

    eso esta malo
    si puede lo arregla

--- Entrega/msjBienvenida.asm
+++ Entrega/msjBienvenida.asm
@@ -1,5 +1,9 @@
 TITLE ESTADÍSTICAS						(main.asm)
 
+;-----------------------------------------------------------------------------------------------------------
+; BACKUP
+;-----------------------------------------------------------------------------------------------------------
+
 ; Descripción:
 ; Este software fue desarrollado para la clase de Arquitectura de Computadores 2014-I
 ; Esta diseñado para generar diferentes estadísticas a partir de datos contenidos en un archivo de texto
@@ -9,24 +13,18 @@ INCLUDE Irvine32.INC
 
 .data
 
-CuadrosAscii BYTE 176,0,177,0,178,0,219,0,219,0,178,0,177,0,176,0
-contadorBarra BYTE 0 ; contador para animaciones
-mensaje DWORD 12 DUP(0)	; array que tiene la dirección donde empieza cada una de las líneas del mensaje
-numLineasMsjBv = 11
-;líneas del mensaje de bienvenida 
-;con espacios en blanco para que cada mensaje se vea centrado
-mensajeBienvenida1 BYTE			       17 DUP(" "),"Bienvenido al programa de estudio estad",161d,"stico",0						;45 caracteres
-mensajeBienvenida2 BYTE			       " ",0																					;1 caracter
-mensajeBienvenida3 BYTE				   23 DUP(" "),"Arquitectura del Computador 2014-I",0										;34 caracteres 
-mensajeBienvenida4 BYTE			       " ",0																					;1 caracter   
-mensajeBienvenida5 BYTE				   22 DUP(" "),"Este software fue desarrollado por:",0										;35 caracteres
-mensajeBienvenida6 BYTE				   17 DUP(" "),"Gustavo Le",162d,"n Preciado Jim",130d,"nez C.C. 1037635880",0				;45 caracteres
-mensajeBienvenida7 BYTE				   16 DUP(" "),"Gustavo Andr",130d,"s Angarita Vel",160d,"squez C.C. 1037635327",0			;48 caracteres
-mensajeBienvenida8 BYTE			       " ",0																					;1 caracter
-mensajeBienvenida9 BYTE				   24 DUP(" "),"Este software est",160d," dise",164d,"ado para calcular",0					;31 caracteres
-mensajeBienvenida10 BYTE			   20 DUP(" "),"distintas medidas estad",161d,"sticas a partir",0							;39 caracteres
-mensajeBienvenida11 BYTE			   27 DUP(" "),"de los datos de un archivo",0												;26 caracteres
-mensajeBienvenida12 BYTE			   10 DUP(" "), 60 DUP("-"),0; indica el final del mensaje.									;60 caracteres
+CuadrosAscii BYTE 176,0,177,0,178,0,219,0
+interruptor BYTE 0 ; interuptor para animaciones
+mensaje DWORD 9 DUP(0)	; array que tiene la dirección donde empieza cada una de las líneas del mensaje
+mensajeBienvenida1 BYTE			       17 DUP(" "),"Bienvenido al programa de estudio estad",161d,"stico",0 ;líneas del mensaje de bienvenida 
+mensajeBienvenida2 BYTE				   23 DUP(" "),"Arquitectura del Computador 2014-I",0		    ;con espacios en blanco para que cada mensaje se vea centrado
+mensajeBienvenida3 BYTE				   22 DUP(" "),"Este software fue desarrollado por:",0
+mensajeBienvenida4 BYTE				   18 DUP(" "),"Gustavo Le",162d,"n Preciado Jim",130d,"nez C.C 1037635880",0
+mensajeBienvenida5 BYTE				   16 DUP(" "),"Gustavo Andr",130d,"s Angarita Vel",160d,"squez C.C 1037635327",0
+mensajeBienvenida6 BYTE				   19 DUP(" "),"Este software est",160d," dise",164d,"ado para calcular",0
+mensajeBienvenida7 BYTE				   20 DUP(" "),"distintas medidas estad",161d,"sticas a partir",0
+mensajeBienvenida8 BYTE				   9 DUP(" "),"de los datos de un archivo y la selecci",162d,"n de la medida deseada",0
+mensajeBienvenida9 BYTE				   10 DUP(" "), 60 DUP("-"),0; indica el final del mensaje.
 contadorMensaje DWORD 0 ;servirá para desplazar el mensaje de bienvenida
 
 auxCiclos	DWORD 5 DUP(0); array que servirá como variables auxiliares y de control durante los ciclos
@@ -48,7 +46,6 @@ mov eax, colores1
 call SetTextColor
 call Clrscr
 call pintarBarras
-call cargarMensaje
 inicio:
 call mostrarMensaje
 JMP INICIO
@@ -56,10 +53,11 @@ JMP INICIO
 main ENDP
 
 ;-----------------------------------------------------------------------------------------------------------
-cargarMensaje PROC
+mostrarMensaje PROC
 ;Muestra el mensaje de bienvenida en 6 líneas entre dos barras animadas, las 6 líneas se van desplazando para 
 ;mostrar todo el mensaje de bienvenida
 ;-----------------------------------------------------------------------------------------------------------
+; Muevo las direcciones al array
 MOV mensaje,OFFSET mensajeBienvenida1 
 MOV [mensaje+4],OFFSET mensajeBienvenida2
 MOV [mensaje+8],OFFSET mensajeBienvenida3
@@ -69,18 +67,6 @@ MOV [mensaje+20],OFFSET mensajeBienvenida6
 MOV [mensaje+24],OFFSET mensajeBienvenida7
 MOV [mensaje+28],OFFSET mensajeBienvenida8
 MOV [mensaje+32],OFFSET mensajeBienvenida9
-MOV [mensaje+36],OFFSET mensajeBienvenida10
-MOV [mensaje+40],OFFSET mensajeBienvenida11
-MOV [mensaje+44],OFFSET mensajeBienvenida12
-cargarMensaje ENDP
-
-;-----------------------------------------------------------------------------------------------------------
-mostrarMensaje PROC
-;Muestra el mensaje de bienvenida en 6 líneas entre dos barras animadas, las 6 líneas se van desplazando para 
-;mostrar todo el mensaje de bienvenida
-;-----------------------------------------------------------------------------------------------------------
-; Muevo las direcciones al array
-
 
 CALL Clrscr ; limpiar la pantalla
 
@@ -102,25 +88,25 @@ CICLO: ; en este ciclo se imprimen las líneas dentro de  las barras
 	CALL WriteString ;imprime la línea del mensaje
 	CALL Crlf
 
-	CMP auxCiclos,numLineasMsjBv ; controla que el valor de auxCiclos siempre este entre 0 y el número de líneas del mensaje
-	JL MENOR
+	CMP auxCiclos,8 ; controla que el valor de auxCiclos siempre este entre 0 y 8
+	JL MENOR8
 	MOV auxCiclos,0
-	JMP NoMENOR
-	MENOR:
+	JMP NoMENOR8
+	MENOR8:
 	INC auxCiclos
-	NoMENOR:
+	NoMENOR8:
 
 LOOP CICLO
 
 CALL Crlf
 
-cmp contadorMensaje,numLineasMsjBv ; controla que el valor de contadorMensaje siempre este entre 0 y el número de líneas del mensaje
-JL MEN
+cmp contadorMensaje,8 ; controla que el valor de contadorMensaje siempre este entre 0 y 8
+JL MEN8
 MOV contadorMensaje,0
-JMP NoMEN
-MEN:
+JMP NoMEN8
+MEN8:
 INC contadormensaje
-NoMEN:
+NoMEN8:
 
 CALL animarBarra
 
@@ -159,32 +145,19 @@ pintarBarras ENDP
 pintarBarra PROC
 ;Procedimiento auxiliar para pintarBarras este pinta cada una de las barras cada que es llamado
 ;-----------------------------------------------------------------------------------------------------------
-MOV	ecx, 10 ; el ciclo se realizará 20 veces para llegar a 80 caracteres
+MOV	ecx, 20 ; el ciclo se realizará 20 veces para llegar a 80 caracteres
 	
 CiloImprimir:
 		
-	MOV edx, OFFSET CuadrosAscii;
+	MOV edx, OFFSET CuadrosAscii; caracter 176
+		
 	CALL	WriteString
-	MOV edx, OFFSET CuadrosAscii[2];
 	CALL	WriteString
-	MOV edx, OFFSET CuadrosAscii[4];
 	CALL	WriteString
-	MOV edx, OFFSET CuadrosAscii[6];
 	CALL	WriteString
 
 	MOV ax, tiempoEspera 
-	CALL delay
-
-	MOV edx, OFFSET CuadrosAscii[8];
-	CALL	WriteString
-	MOV edx, OFFSET CuadrosAscii[10];
-	CALL	WriteString
-	MOV edx, OFFSET CuadrosAscii[12];
-	CALL	WriteString
-	MOV edx, OFFSET CuadrosAscii[14];
-	CALL	WriteString
-
-	MOV ax, tiempoEspera 
+	
 	CALL delay
 	
 	
@@ -200,22 +173,35 @@ animarBarra PROC
 ;-----------------------------------------------------------------------------------------------------------
 
 
-MOV cx,80 ; hará un ciclo 40 veces pintando de a dos caracteres 176 o 177
+MOV cx,10 ; hará un ciclo 40 veces pintando de a dos caracteres 176 o 177
 L1:
-	
-	MOV eax, DWORD PTR contadorBarra
-	MOV edx, [DWORD PTR CuadrosAscii + eax]
-	CALL	WriteString
-	CMP contadorBarra, 14
-	JL menor14
-		MOV contadorBarra, 0
-		JMP finsi
-	menor14:
-		INC contadorBarra
-		INC contadorBarra
-	finsi:
+	CMP interruptor,2 ;escoge qué caracter usar para pintar la barra
+	MOV edx, OFFSET CuadrosAscii
+	JG FINSI
+		ADD edx,2
+	FINSI:
+		CALL	WriteString
+		CALL	WriteString
+		CALL	WriteString
+		CALL	WriteString
+		CALL	WriteString
+		CALL	WriteString
+		CALL	WriteString
+		CALL	WriteString
 
 LOOP L1 ;fin cilo para pintar la barra
+
+CMP interruptor,2 ; Cambia el estado de la variable de seleccion del caracter
+JL SINO2
+	CMP interruptor,3
+	JLE SINO2
+		MOV interruptor,0
+	
+JMP FINSI2
+SINO2:
+		INC interruptor
+FINSI2:
+		
 		
 RET
 animarBarra ENDP
